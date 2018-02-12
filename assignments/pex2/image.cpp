1dfacbae8249b75c112fc244f82514e1d253521f
Joshua Tate <jmtate@icloud.com>
2018-02-11 21:44:30 -0800

    Assignment 2: Changed deallocatePixels

--- assignments/pex2/image.cpp
+++ assignments/pex2/image.cpp
@@ -73,7 +73,7 @@ void Image::allocatePixels() {
 void Image::deallocatePixels() {
     if (pixels) {
 	for (int i = 0; i < rows; i++)
-	    delete pixels[i];
+	    delete [] pixels[i];
 	delete [] pixels;
     }
 }
