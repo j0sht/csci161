e3ed4c66369e64cfe40b1bed0b3f9b89ace9fea5
Joshua Tate <jmtate@icloud.com>
2018-02-17 16:55:00 -0800

    Chapter 11: Added ifndef

--- absolute-c++/c11-multiplefiles/dtime.h
+++ absolute-c++/c11-multiplefiles/dtime.h
@@ -2,6 +2,8 @@
 // This is the interface for the class DigitalTime.
 // Values of this type are times of day. The values are input and output
 //  in 24-hour notation, as in 9:30 for 9:30 AM and 14:45 for 2:45 PM.
+#ifndef DTIME_H
+#define DTIME_H
 #include <iostream>
 using namespace std;
 
@@ -36,3 +38,4 @@ private:
     //  returns 3.
     static int digitToInt(char c);
 };
+#endif // DTIME_H
