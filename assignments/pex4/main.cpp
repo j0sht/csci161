1f7a928ebf3ccfc5ab86e6deab1f2a7b5949620b
Joshua Tate <jmtate@icloud.com>
2018-03-20 21:34:20 -0700

    Refactored main: added new functions

--- assignments/pex4/main.cpp
+++ assignments/pex4/main.cpp
@@ -11,6 +11,7 @@
 #include "urlstack.h"
 #include <iostream>
 #include <cstdlib>
+#include <algorithm> // for transform
 using namespace std;
 
 const string BACK = "back";
@@ -19,7 +20,15 @@ const string CLICK = "click";
 const string EXIT = "exit";
 const string PROMPT = "command$ ";
 
+// Converts string to lower case
+void convertToLowercase(string& s);
+// Displays top URL of stack
 void displayCurrentURL(const URLStack& stack);
+// Pop's url from s1 and pushes it onto s2, then displays top of s1 if n == 1,
+//  or top of s2 if n == 2, otherwise displays nothing
+void popPushDisplay(URLStack& s1, URLStack & s2, int n) throw (EmptyStack);
+// Removes trailing input from cin
+void removeTrailingInput();
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -33,39 +42,46 @@ int main(int argc, char** argv) {
     do {
 	cout << PROMPT;
 	cin >> userInput;
-	if (userInput == BACK) {
+	convertToLowercase(userInput);
+	if (userInput == BACK)
 	    if (backStack.getCount() == 1)
 		cout << "Can't move back any more.\n";
-	    else {
-		URLRef url = backStack.pop();
-		forwardStack.push(url);
-		displayCurrentURL(backStack);
-	    }
-	} else if (userInput == FWD) {
+	    else
+		popPushDisplay(backStack, forwardStack, 1);
+	else if (userInput == FWD)
 	    try {
-		URLRef url = forwardStack.pop();
-		backStack.push(url);
-		displayCurrentURL(backStack);
+		popPushDisplay(forwardStack, backStack, 2);
 	    } catch (EmptyStack& e) {
 		cout << "Can't move forward any more.\n";
 	    }
-	} else if (userInput == CLICK) {
+	else if (userInput == CLICK) {
 	    URLRef url = *(new URL);
 	    cin >> url;
-	    string garbage;
-	    getline(cin, garbage);
 	    backStack.push(url);
 	    forwardStack.deleteAll();
 	    displayCurrentURL(backStack);
-	} else if (userInput != EXIT) {
+	} else if (userInput != EXIT)
 	    cout << "Uknown command, try again.\n";
-	    string garbage;
-	    getline(cin, garbage);
-	}
+	removeTrailingInput();
     } while (userInput != EXIT);
     return 0;
 }
 
+void convertToLowercase(string& s) {
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+}
 void displayCurrentURL(const URLStack& stack) {
     cout << "You are viewing: " << stack.peek() << endl;
+}
+void popPushDisplay(URLStack& s1, URLStack& s2, int n) throw (EmptyStack) {
+    URLRef url = s1.pop();
+    s2.push(url);
+    if (n == 1)
+	displayCurrentURL(s1);
+    else if (n == 2)
+	displayCurrentURL(s2);
+}
+void removeTrailingInput() {
+    string garbage;
+    getline(cin, garbage);
 }
